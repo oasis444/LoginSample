8f31088ba8a58e3599cd97b1fd9a118891cb2738
jiwook_han <jiwook.han.dev@gmail.com>
2023-05-04 00:46:46 +0900

    Initial Commit

---
+++ LoginSample/LoginVC.swift
@@ -0,0 +1,37 @@
+//
+//  LoginVC.swift
+//  LoginSample
+//
+//  Copyright (c) 2023 oasis444. All right reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    @IBOutlet weak var emailLoginButton: UIButton!
+    @IBOutlet weak var googleLoginButton: UIButton!
+    @IBOutlet weak var appleLoginButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        [emailLoginButton, googleLoginButton, appleLoginButton].forEach {
+            $0?.layer.borderWidth = 1
+            $0?.layer.borderColor = UIColor.white.cgColor
+            $0?.layer.cornerRadius = 30
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        navigationController?.navigationBar.isHidden = true
+    }
+    
+    @IBAction func googleLoginTapped(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func appleLoginTapped(_ sender: UIButton) {
+        
+    }
+}
